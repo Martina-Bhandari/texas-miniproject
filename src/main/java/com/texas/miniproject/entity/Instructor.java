92ef04d533f4b7ebd5eb5c426c5e0052184829c7
Martina Bhandari <martinabhandari2001@gmail.com>
2025-02-28 08:53:39 +0545

    updated

--- src/main/java/com/texas/miniproject/entity/Instructor.java
+++ src/main/java/com/texas/miniproject/entity/Instructor.java
@@ -1,4 +1,22 @@
 package com.texas.miniproject.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "instructor")
 public class Instructor {
+    @Id
+    @SequenceGenerator(name = "instructor_sequence", sequenceName = "instructor_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "instructor_sequence")
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private Long departmentId;
 }
